16688d5f133e88d41800058e8793528183ab36a6
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-05 05:26:43 +0700

    lenovo_huyhai1994 update exception class

---
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/exception/OurException.java
@@ -0,0 +1,7 @@
+package com.codegym.hotelmanagementsystemcodegymmodule4.exception;
+
+public class OurException extends RuntimeException {
+    public OurException(String message) {
+        super(message);
+    }
+}
