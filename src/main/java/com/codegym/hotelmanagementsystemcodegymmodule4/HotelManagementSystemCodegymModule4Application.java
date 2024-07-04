e8df0f6696153136e185e37faec509722c33b509
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-05 04:56:57 +0700

    lenovo_huyhai1994 update project application.properties

---
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/HotelManagementSystemCodegymModule4Application.java
@@ -0,0 +1,13 @@
+package com.codegym.hotelmanagementsystemcodegymmodule4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemCodegymModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementSystemCodegymModule4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/hotelmanagementsystemcodegymmodule4/HotelManagementSystemCodegymModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.hotelmanagementsystemcodegymmodule4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemCodegymModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
