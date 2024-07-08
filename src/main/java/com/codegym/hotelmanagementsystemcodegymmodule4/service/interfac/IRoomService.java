5b73944e95892209a9dbe8ee1be70c515b786a6c
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-08 12:31:07 +0700

    lenovo_huyhai1994 update project

--- src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/service/interfac/IRoomService.java
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/service/interfac/IRoomService.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
+
 public interface IRoomService {
 
     Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description);
