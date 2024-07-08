305384c0201990bc5047aa2fbae141b1c15bc4bb
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-08 14:06:00 +0700

    lenovo_huyhai1994 update SecurityInit

---
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.hotelmanagementsystemcodegymmodule4;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
