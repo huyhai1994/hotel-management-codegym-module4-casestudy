953802a421da7038164065b14cb57f2bb69633c9
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-08 13:00:13 +0700

    lenovo_huyhai1994 update UserService (created repository bean)

--- src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/config/UserService.java
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/config/UserService.java
@@ -1,4 +1,21 @@
 package com.codegym.hotelmanagementsystemcodegymmodule4.config;
 
-public class UserService implements UserDetailsService{
+import com.codegym.hotelmanagementsystemcodegymmodule4.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/*TODO s-step2: implement the UserDetailsService (after import the security librery)*/
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return null;
+    }
 }
--- src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/repository/UserRepository.java
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/*TODO: s-step 3 creating user interface repository */
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
