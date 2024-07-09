59882f4c194180ee466ce32a7aa6a515c3ff28fe
HaNam94 <haduynam.94@gmail.com>
2024-07-09 17:22:51 +0700

    nam update

--- src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/service/impl/UploadService.java
+++ src/main/java/com/codegym/hotelmanagementsystemcodegymmodule4/service/impl/UploadService.java
@@ -50,7 +50,7 @@ public class UploadService {
 
     // upload file lên firebase
     private String uploadFileFromServerToFirebase(String filePath) {
-        Path localPath = Paths.get(filePath); // lấy ra đối tượng Paths của ảnh vừa upload lên server
+       /* Path localPath = Paths.get(filePath); // lấy ra đối tượng Paths của ảnh vừa upload lên server
         String fileName = localPath.getFileName().toString(); // lấy ra tên file upload
 
         BlobId blobId = BlobId.of(bucketName, fileName); // tạo file trên storage bằng tên và bucketname chỉ đinh
@@ -65,6 +65,7 @@ public class UploadService {
             return blob.getMediaLink(); // trả về đường dẫn ảnh online
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
+        return "";
     }
 }
