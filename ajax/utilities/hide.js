8c9ccba21408fb9ce757d1b9266ea29f16e25a5a
HaNam94 <haduynam.94@gmail.com>
2024-07-11 09:45:02 +0700

    fixed hide add room (USER_ROLE)

--- ajax/utilities/hide.js
+++ ajax/utilities/hide.js
@@ -20,3 +20,12 @@ function showLogout(){
 function hideLogout(){
     $('#logout').hide();
 }
+
+function hideAddRoom(){
+    event.preventDefault();
+        let dataUser = JSON.parse(localStorage.getItem('token'));
+        console.log("this is key"+localStorage.getItem('token'));
+        if (dataUser && dataUser.authorities && dataUser.authorities[0].authority === "ROLE_USER") {
+            $('.btn-primary').hide();
+        }
+}
