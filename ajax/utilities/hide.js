882c05bf785b4a2767ed8ab18901e70621afc8f5
HaNam94 <haduynam.94@gmail.com>
2024-07-11 10:08:55 +0700

    refactory navbar (remove search)

--- ajax/utilities/hide.js
+++ ajax/utilities/hide.js
@@ -22,10 +22,9 @@ function hideLogout(){
 }
 
 function hideAddRoom(){
-    event.preventDefault();
         let dataUser = JSON.parse(localStorage.getItem('token'));
-        console.log("this is key"+localStorage.getItem('token'));
-        if (dataUser && dataUser.authorities && dataUser.authorities[0].authority === "ROLE_USER") {
+        if (dataUser && dataUser.authorities && dataUser.authorities[0].authority === "ROLE_USER"){
             $('.btn-primary').hide();
         }
+
 }
