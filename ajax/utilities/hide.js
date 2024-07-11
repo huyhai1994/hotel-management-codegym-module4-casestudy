7d64c8935f4ae8b5e905570edd3d2d40e71f3203
HaNam94 <haduynam.94@gmail.com>
2024-07-11 09:20:48 +0700

    fixed login, loggout

---
+++ ajax/utilities/hide.js
@@ -0,0 +1,22 @@
+
+function showSignIn(){
+
+}
+function hideSignIn (){
+    $('#signin').hide();
+}
+function showSignUp(){
+
+}
+function hideSignUp(){
+$('#signup').hide();
+}
+function hideSignOut(){
+
+}
+function showLogout(){
+
+}
+function hideLogout(){
+    $('#logout').hide();
+}
